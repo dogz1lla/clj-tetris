ec130acc902c8fc70a7144c16e0179e86adf82c4
Grigorii Zarubin <zarubin.grigory@gmail.com>
2021-07-25 03:04:02 +0200

    2d coord for pieces

--- src/tetris/main.clj
+++ src/tetris/main.clj
@@ -38,7 +38,8 @@
 ;;       non-trivial pieces; implementation: replaced plain integer with set
 ;;       as the first value of the game state vector
 ;; DONE: write logic for pieces of non-trivial shapes
-;; TODO: need to use 2d coordinates after all (for wall overlap checks)
+;; DONE: need to use 2d coordinates after all (for wall overlap checks)
+;; TODO: prevent wall overlaps on piece spawn
 
 ;(defn complete-row? [row]
 ;  (every? true? row))
@@ -52,14 +53,19 @@
   (rand-int width))
 
 (def empty-bucket #{})
-(def init-state [(p/single-piece (pick-x)) empty-bucket])
+;;(def init-state [(p/single-piece (pick-x) 0) empty-bucket])
+(def init-state [(p/square-piece (pick-x) 0 width) empty-bucket])
 (def game-history (atom [init-state]))
+
+(defn fall 
+  [piece]
+  (assoc piece 1 (inc (last piece))))
 
 (defn move-piece 
   "Make the piece fall by one unit towards bottom."
   [game-state]
-  (let [current-positions (first game-state)]
-    (assoc game-state 0 (into #{} (map #(+ width %) current-positions)))))
+  (let [pieces (first game-state)]
+    (assoc game-state 0 (into #{} (map fall pieces)))))
 
 (defn occupied? 
   "Check if the next space in piece's way is already taken."
@@ -68,17 +74,17 @@
 
 (defn piece-at-bottom? 
   "Check if the piece reached the bottom of the bucket."
-  [positions]
-  (not-every? false? (map #(<= (- max-idx %) width) positions)))
+  [pieces]
+  (not-every? true? (map #(< (last %) (dec height)) pieces)))
 
 (defn piece-sinked? 
   "Check if the piece is 'sinked' i.e., it is either at the bottom or 
   the next space is occupied."
   [game-state]
-  (let [[piece field] game-state
-        piece-shift (into #{} (map #(+ width %) piece))]
+  (let [[pieces field] game-state
+        piece-shift (into #{} (map fall pieces))]
     (or (occupied? piece-shift field)
-        (piece-at-bottom? piece))))
+        (piece-at-bottom? pieces))))
 
 (defn freeze-piece 
   "When piece is sinked it becomes a part of the static content of the bucket."
@@ -89,13 +95,17 @@
 (defn spawn-piece 
   "Spawn a new piece at the top of the bucket."
   [game-state]
-  (let [new-piece (p/single-piece (pick-x))]
+  ;;(let [new-piece (p/single-piece (pick-x) 0)]
+  ;;(let [new-piece (p/square-piece (pick-x) 0 width)]
+  (let [new-piece (p/gamma-piece (pick-x) 0 width)]
     (assoc game-state 0 new-piece)))
 
 (defn game-over? 
   "Game ends when there are no free places anymore."
   [game-state]
-  (empty? (cs/difference (set all-slots) (last game-state))))
+  ;;(empty? (cs/difference (set all-slots) (last game-state))))
+  ;;(<= max-idx (count (last game-state))))
+  (<= 100 (count (last game-state))))
 
 (defn game-step 
   "Advance the game by one step. 
@@ -111,8 +121,9 @@
   "Turn game state in a list of 0s and 1s."
   [game-state]
   (let [[piece field] game-state
-        all-cells (cs/union field piece)]
-    (for [i all-slots] (if (all-cells i) 1 0))))
+        all-cells (cs/union field piece)
+        linearized-state (into #{} (map #(+ (* width (last %)) (first %)) all-cells))]
+    (for [i all-slots] (if (linearized-state i) 1 0))))
 
 (defn visualize-row
   "Turn a row of the vectorized state into a string of Xs and Os."
@@ -133,6 +144,7 @@
   (while (not (game-over? (last @game-history)))
     #_(Thread/sleep 1000)
     #_(print-state (vectorize-state (last @game-history)))
+    #_(println (last @game-history))
     (swap! game-history #(conj % (game-step (last @game-history)))))
   @game-history)
 
@@ -198,4 +210,4 @@
   :draw draw
   :features [:keep-on-top]
   :size [(:lx box) (:ly box)]
-  :on-close #(println "closed!"))
+  :on-close #(println (str "closed! " (count (last (last @game-history))))))
--- src/tetris/pieces.clj
+++ src/tetris/pieces.clj
@@ -9,34 +9,38 @@
   (> 0 idx))
 
 (defn overlap-right-wall?
-  [idx width]
-  (>= idx width))
+  [pieces width]
+  (not-every? false? (map #(< (first %) width) pieces)))
 
 (defn overlap-left-wall?
-  [idx]
-  (> 0 idx))
+  [pieces]
+  (not-every? false? (map #(< (first %) 0) pieces)))
 
 (defn overlap? 
-  [piece width]
-  not-every? false? (map #(<= (- max-idx %) width) piece))
+  [pieces width]
+  (or (overlap-left-wall? pieces)
+      (overlap-right-wall? pieces width)))
 
 (defn single-piece
-  [p0]
-  #{p0})
+  [x0 y0]
+  #{[x0 y0]})
 
 (defn square-piece 
-  [p0 width]
-  (let [p1 (inc p0)
-        p2 (+ width p0)
-        p3 (+ width p1)] 
-    [p0 #{p1 p2 p3}]))
+  [x0 y0 width]
+  (let [p0 [x0 y0]
+        p1 [(inc x0) y0]
+        p2 [x0 (inc y0)]
+        p3 [(inc x0) (inc y0)]
+        square #{p0 p1 p2 p3}]
+    square))
 
 (defn gamma-piece 
-  [p0 width]
-  (let [p1 (inc p0)
-        p2 (inc p1)
-        p3 (+ width p0)] 
-    [p0 #{p1 p2 p3}]))
+  [x0 y0 width]
+  (let [p0 [x0 y0]
+        p1 [(inc x0) y0]
+        p2 [(+ x0 2) y0]
+        p3 [x0 (inc y0)]] 
+    #{p0 p1 p2 p3}))
 
 (defn gamma-piece-mirror
   [p0 width]
